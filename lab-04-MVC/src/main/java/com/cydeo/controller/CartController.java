8f633231fae036eb01c89628d86be1f19a1373df
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-10 20:59:57 +0300

    lab-04-MVC updated

--- lab-04-MVC/src/main/java/com/cydeo/controller/CartController.java
+++ lab-04-MVC/src/main/java/com/cydeo/controller/CartController.java
@@ -1,16 +1,11 @@
 package com.cydeo.controller;
 
-import com.cydeo.model.Cart;
 import com.cydeo.model.CartItem;
-import com.cydeo.model.Product;
 import com.cydeo.service.impl.CartServiceImpl;
-import com.cydeo.service.impl.ProductServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.List;
 import java.util.UUID;
 

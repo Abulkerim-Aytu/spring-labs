e7d20561d9eddf7cd8d0b1104e948f0ed5416a92
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-10 20:20:39 +0300

    lab-04-MVC updated

--- lab-04-MVC/src/main/java/com/cydeo/controller/ProductController.java
+++ lab-04-MVC/src/main/java/com/cydeo/controller/ProductController.java
@@ -1,7 +1,27 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Product;
+import com.cydeo.service.impl.CartServiceImpl;
+import com.cydeo.service.impl.ProductServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
+
+    @RequestMapping("/search-product/{product}")
+    public String searchProduct(@PathVariable String product, Model model){
+
+        ProductServiceImpl search = new ProductServiceImpl();
+        List<Product> result = search.searchProduct(product);
+
+        model.addAttribute("productList",result);
+        model.addAttribute("product",result);
+
+        return "/product/product-list";
+    }
 }

3893120759dbab022633f235a423d8ac05c82d4d
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-10 09:44:35 +0300

    lab-01-ioc updated and complited

--- lab-01-ioc/src/main/java/com/cydeo/config/Config.java
+++ lab-01-ioc/src/main/java/com/cydeo/config/Config.java
@@ -6,17 +6,39 @@ import com.cydeo.account.Saving;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 @Configurable
 public class Config {
 
     @Bean
-    public Current initializeCur (){
-        return new Current();
+    public Currency initializecy() {
+        Currency inital = new Currency();
+        inital.setCode("12");
+        return inital;
     }
 
     @Bean
-    public Saving initializeSa(){
-        return new Saving();
+    public Current initializeCur() {
+        Current inital = new Current();
+        inital.setCurrency(initializecy());
+        inital.setAmount(BigDecimal.ONE);
+        inital.setAccountId(UUID.randomUUID());
+        return inital;
+    }
+
+    ;
+
+    @Bean
+    public Saving initializeSa() {
+
+        Saving inital = new Saving();
+        inital.setCurrency(initializecy());
+        inital.setAmount(BigDecimal.TEN);
+        inital.setAccountId(UUID.randomUUID());
+
+        return inital;
     }
 
 

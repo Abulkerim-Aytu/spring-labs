71573d2c9b2668e6a593fe3003a88b16f31cf81b
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-08-21 21:16:48 +0300

    lab-04-MVC updated

--- lab-04-MVC/src/main/java/com/cydeo/controller/ProductController.java
+++ lab-04-MVC/src/main/java/com/cydeo/controller/ProductController.java
@@ -1,8 +1,10 @@
 package com.cydeo.controller;
 
 import com.cydeo.model.Product;
+import com.cydeo.service.ProductService;
 import com.cydeo.service.impl.CartServiceImpl;
 import com.cydeo.service.impl.ProductServiceImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+//    Here wu use the dependency injection.
 
     @RequestMapping("/search-product/{product}")
     public String searchProduct(@PathVariable String product, Model model){
 
-        ProductServiceImpl search = new ProductServiceImpl();
-        List<Product> result = search.searchProduct(product);
+//        ProductServiceImpl search = new ProductServiceImpl();
+
+        List<Product> result = productService.searchProduct(product);
 
         model.addAttribute("productList",result);
         model.addAttribute("product",result);

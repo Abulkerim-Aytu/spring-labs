a2e3e178946391c8ed70f9a5196fb2e728afc6f4
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-14 18:53:09 +0300

    lab-06-orm Completed And Finished

--- lab-06-orm/src/main/java/com/cydeo/entity/Orders.java
+++ lab-06-orm/src/main/java/com/cydeo/entity/Orders.java
@@ -1,10 +1,13 @@
 package com.cydeo.entity;
 
-import lombok.AllArgsConstructor;
+import com.cydeo.enums.PaymentMethod;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
 
 @Entity
 @Data
@@ -12,4 +15,20 @@ import javax.persistence.Entity;
 
 
 public class Orders extends BaseEntity{
+
+    private BigDecimal paidPrice;
+    private BigDecimal totalPrice;
+
+    @ManyToOne
+    private Customer customer;
+
+    @OneToOne
+    private Cart cart;
+
+    @OneToOne
+    private Payment payment;
+    public Orders(BigDecimal paidPrice, BigDecimal totalPrice) {
+        this.paidPrice = paidPrice;
+        this.totalPrice = totalPrice;
+    }
 }
--- lab-06-orm/src/main/java/com/cydeo/entity/Payment.java
+++ lab-06-orm/src/main/java/com/cydeo/entity/Payment.java
@@ -1,10 +1,14 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.PaymentMethod;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.math.BigDecimal;
 
 @Entity
 @Data
@@ -12,5 +16,12 @@ import javax.persistence.Entity;
 
 
 public class Payment extends BaseEntity{
+    private BigDecimal paidPrice;
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
 
+    public Payment(BigDecimal paidPrice, PaymentMethod paymentMethod) {
+        this.paidPrice = paidPrice;
+        this.paymentMethod = paymentMethod;
+    }
 }

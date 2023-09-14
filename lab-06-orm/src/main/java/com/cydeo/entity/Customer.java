d4e7e0afdf11c4e34cba06770a846302e8f83e08
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-09-14 16:17:13 +0300

    lab-06-orm Begin

---
+++ lab-06-orm/src/main/java/com/cydeo/entity/Customer.java
@@ -0,0 +1,13 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+
+public class Customer extends BaseEntity{
+}

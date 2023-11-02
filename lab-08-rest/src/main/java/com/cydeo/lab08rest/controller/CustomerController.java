0d05bfd1a3dc7327127b5a3a4692241edd85134d
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-11-02 14:56:06 +0300

    lab-08-rest-update-8

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/CustomerController.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/CustomerController.java
@@ -1,11 +1,48 @@
 package com.cydeo.lab08rest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cydeo.lab08rest.dto.ResponseWrapper;
+import com.cydeo.lab08rest.service.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
 
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getCustomers(){
+        return ResponseEntity.ok(new ResponseWrapper("true",customerService.listAllCustomer()));
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<ResponseWrapper> getAddressListByStartsWithAddress(@PathVariable("email") String email){
+        return ResponseEntity.ok(new ResponseWrapper("true",customerService.listAllCustomerByEmail(email)));
+    }
+//
+//    @GetMapping("/customer/{id}")
+//    public ResponseEntity<ResponseWrapper> getAddressListByStartsWithAddress(@PathVariable("id") Long id){
+//        return ResponseEntity.ok(new ResponseWrapper("true",addressService.listAllAddressByCustomerId(id)));
+//    }
+//
+//    @GetMapping("/customer/{customerId}/name/{name}")
+//    public ResponseEntity<ResponseWrapper> getAddressListByCustomerAndCustomerName(@PathVariable("customerId") Long customerId, @PathVariable("name") String name){
+//        return ResponseEntity.ok(new ResponseWrapper("true",addressService.listAllAddressByCustomerIdAndName(customerId,name)));
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<ResponseWrapper> createAddress(@RequestBody AddressDTO addressDTO){
+//        return ResponseEntity.ok(new ResponseWrapper("true", addressService.createAddress(addressDTO)));
+//    }
+//
+//    @PutMapping
+//    public ResponseEntity<ResponseWrapper> updateAddress(@RequestBody AddressDTO addressDTO){
+//        return ResponseEntity.ok(new ResponseWrapper("true", addressService.updateAddress(addressDTO)));
+//    }
 
 }
--- lab-08-rest/src/main/java/com/cydeo/lab08rest/service/CustomerService.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/CustomerService.java
@@ -1,4 +1,15 @@
 package com.cydeo.lab08rest.service;
 
+import com.cydeo.lab08rest.dto.CustomerDTO;
+
+import java.util.List;
+
 public interface CustomerService {
+    List<CustomerDTO> listAllCustomer();
+    CustomerDTO listAllCustomerByEmail(String email);
+    CustomerDTO updateCustomer(CustomerDTO customerDTO);
+    CustomerDTO createCustomer(CustomerDTO customerDTO);
+
+
+
 }

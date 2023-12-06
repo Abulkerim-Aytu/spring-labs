13bf9b6217fe29cd4ea79a9b72f3db1fdd4a1252
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-12-06 13:23:16 +0300

    lab-09-Testing-update-4

--- lab-09-Testing/src/test/java/com/cydeo/lab09rest/service/unit/CartServiceImplUnitTest.java
+++ lab-09-Testing/src/test/java/com/cydeo/lab09rest/service/unit/CartServiceImplUnitTest.java
@@ -1,7 +1,12 @@
 package com.cydeo.lab09rest.service.unit;
 
+import com.cydeo.lab09rest.entity.Cart;
+import com.cydeo.lab09rest.entity.CartItem;
 import com.cydeo.lab09rest.entity.Customer;
 import com.cydeo.lab09rest.entity.Product;
+import com.cydeo.lab09rest.enums.CartState;
+import com.cydeo.lab09rest.repository.CartItemRepository;
+import com.cydeo.lab09rest.repository.CartRepository;
 import com.cydeo.lab09rest.repository.ProductRepository;
 import com.cydeo.lab09rest.service.impl.CartServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -10,9 +15,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 // Unit Testing Works with Mock Data. So instead of using real data we use Mockito to Mock the DATA.
@@ -24,6 +32,12 @@ public class CartServiceImplUnitTest {
 
     @Mock
     private ProductRepository productRepository;
+
+    @Mock
+    private CartRepository cartRepository;
+
+    @Mock
+    private CartItemRepository cartItemRepository;
 
 //    This annotation help us to Wiring bean, just like constructor in the Service/Impl ' classes.
     @InjectMocks
@@ -40,6 +54,7 @@ public class CartServiceImplUnitTest {
 
     @Test
     public void should_throw_exception_when_product_remaining_quantity_is_less_than_quantity(){
+//        These are The dummy Data that we use with Mock repositories, services methods.
         Product product = new Product();
         product.setRemainingQuantity(10);
 
@@ -48,5 +63,71 @@ public class CartServiceImplUnitTest {
         assertThat(throwable).isInstanceOf(RuntimeException.class);
         assertThat(throwable).hasMessage("Not enough stock");
     }
+
+    @Test
+    public void should_add_item_to_cart_when_cart_exists_and_cart_item_exists_in_the_cart(){
+        // Given
+        Product product = new Product();
+        product.setId(1L);
+        product.setRemainingQuantity(10);
+
+        Cart cart = new Cart();
+        cart.setCartState(CartState.CREATED);
+        List<Cart> cartList = new ArrayList<>();
+        cartList.add(cart);
+
+        CartItem cartItem = new CartItem();
+        cartItem.setProduct(product);
+        cartItem.setCart(cart);
+        cartItem.setQuantity(2);
+
+        Customer customer = new Customer();
+        customer.setId(1L);
+
+        // When
+        when(productRepository.findById(product.getId())).thenReturn(Optional.of(product));
+        when(cartRepository.findAllByCustomerIdAndCartState
+                (customer.getId(), CartState.CREATED)).thenReturn(cartList);
+        when(cartItemRepository.findAllByCartAndProduct(cart, product)).thenReturn(cartItem);
+        // Then
+        boolean result = cartService.addToCart(customer, product.getId(), 8);
+        assertTrue(result);
+        assertThat(cartItem.getQuantity()).isEqualTo(10);
+    }
+
+
+    @Test
+    public void should_throw_an_exception_when_cart_list_size_is_two(){
+        Product product = new Product();
+        product.setId(1L);
+        product.setRemainingQuantity(10);
+
+        Cart cart = new Cart();
+        cart.setCartState(CartState.CREATED);
+
+        Cart cart2 = new Cart();
+        cart2.setCartState(CartState.CREATED);
+
+        List<Cart> cartList = new ArrayList<>();
+        cartList.add(cart);
+        cartList.add(cart2);
+
+        Customer customer = new Customer();
+        customer.setId(1L);
+
+        when(productRepository.findById(product.getId())).thenReturn(Optional.of(product));
+        when(cartRepository.findAllByCustomerIdAndCartState
+                (customer.getId(), CartState.CREATED)).thenReturn(cartList);
+
+        Throwable throwable = catchThrowable(() ->
+                cartService.addToCart(customer, 1L, 8));
+        assertThat(throwable).isInstanceOf(RuntimeException.class);
+    }
+
+    // homework
+    // write unit test for scenarios
+    // scenario 1 -> cart doesn't exist for customer or cart list is null (new Arraylist or null)
+    // scenario 2 -> cart exist but cart item doesn't exist
+
 
 }

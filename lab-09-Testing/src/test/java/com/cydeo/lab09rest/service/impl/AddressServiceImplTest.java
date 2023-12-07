b0580aeccb723dc63bbbf609e77329f95f23d0fd
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-12-07 16:51:22 +0300

    lab-09-Testing-Review-update-2

--- lab-09-Testing/src/test/java/com/cydeo/lab09rest/service/impl/AddressServiceImplTest.java
+++ lab-09-Testing/src/test/java/com/cydeo/lab09rest/service/impl/AddressServiceImplTest.java
@@ -1,10 +1,44 @@
 package com.cydeo.lab09rest.service.impl;
 
+import com.cydeo.lab09rest.dto.AddressDTO;
+import com.cydeo.lab09rest.repository.AddressRepository;
+import com.cydeo.lab09rest.service.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 //---------------Unit-Test---------------
 
-
+@ExtendWith(MockitoExtension.class)
 class AddressServiceImplTest {
+
+    @Mock
+    private AddressRepository addressRepository;
+
+    @Mock
+    private CustomerService customerService;
+
+    @InjectMocks
+    private AddressServiceImpl addressService;
+
+//  Address Update Methods
+    @Test
+    public void should_not_update_the_address_when_customer_not_found(){
+        AddressDTO addressDTO = new AddressDTO();
+        addressDTO.setCustomerId(1L);
+
+        when(customerService.existById(addressDTO.getCustomerId())).thenReturn(true);
+        Throwable throwable = catchThrowable(() -> addressService.update(new AddressDTO()));
+        assertThat(throwable).isInstanceOf(RuntimeException.class);
+    }
+
+
 
 }
